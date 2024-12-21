bc84e46092e529396ee99baca693dfd519cb6d5d
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-22 01:48:56 +0200

    firtst commit

---
+++ src/main/java/org/example/simplestspringboottest/SimplestSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.simplestspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplestSpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplestSpringBootTestApplication.class, args);
+    }
+
+}
